f9f412d3299ccbfe0b7148f5fa1209ab701e5ad0
maxdesanta <76093413+maxdesanta@users.noreply.github.com>
2021-03-22 12:04:31 +0700

    Add files via upload

---
+++ js/kalkulator.js
@@ -0,0 +1,90 @@
+let indonesia = 1;
+console.log(indonesia);
+
+let usa = 2;
+console.log(usa);
+
+let britain = 3;
+console.log(britain);
+
+let brazil = 4;
+console.log(brazil);
+
+let rusia = 5;
+console.log(rusia);
+
+let mexico = 6;
+console.log(mexico);
+
+let china = 7;
+console.log(china);
+
+let mesir = 8;
+console.log(mesir);
+
+let japan = 9;
+console.log(japan);
+
+let france = 10;
+console.log(france);
+
+let canada = 11;
+console.log(canada);
+
+let india = 12;
+console.log(india);
+
+let colombia = 13;
+console.log(colombia);
+
+let argentina = 14;
+console.log(argentina);
+
+let ecuador = 15;
+console.log(ecuador);
+
+let italy = 16;
+console.log(italy);
+
+let uruguay = 17;
+console.log(uruguay);
+
+let panama = 18;
+console.log(panama);
+
+let spain = 19;
+console.log(spain);
+
+let australia = 20;
+console.log(australia);
+
+let peru = 21;
+console.log(peru);
+
+let singapura = 22;
+console.log(singapura);
+
+let vietnam = 23;
+console.log(vietnam);
+
+let kamboja = 24;
+console.log(kamboja);
+
+let laos = 25;
+console.log(laos);
+
+let thailand = 26;
+console.log(thailand);
+
+let israel = 27;
+console.log(israel);
+
+let vatikan = 28;
+console.log(vatikan);
+
+let germany = 29;
+console.log(germany);
+
+let angola = 30;
+console.log(angola);
+
